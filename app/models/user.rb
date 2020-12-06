2afc77043d250a55b3484da0e1b658115911ef80
bokuson100 <syk.grazie.50@gmail.com>
2020-12-06 19:06:54 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
+  #ユーザー情報
+  validates :nickname, presence: {message:'ニックネームは必須です。'}
+  validates :uniqueness: {message: 'そのアドレスは既に登録されています。'}
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは6文字以上かつ英数字をそれぞれ含めてください' }
+
+  #本人情報確認
+  validates :first_name_kanji, presence: true
+  validates :last_name_kanji, presence: true 
+  validates :first_name_kana, presence: true
+  validates :last_name_kana,  presence: true
+  validates :birthday,  presence: true
 end
