8b67ad16caa441fcf312ba80581376a989bac315
bokuson100 <syk.grazie.50@gmail.com>
2020-12-13 21:35:32 +0900

    テストデータの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    first_name_kanji      {"山田"}
+    last_name_kanji       {"太郎"}
+    first_name_kana       {"ヤマダ"}
+    last_name_kana        {"タロウ"}
+    birthday              {"2020-11-13"}
   end
 end
