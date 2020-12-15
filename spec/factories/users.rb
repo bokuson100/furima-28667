0687512b87ef586a984d8adc437f9b08459f7e91
bokuson100 <syk.grazie.50@gmail.com>
2020-12-15 18:29:04 +0900

    passwordテストデータの変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'hogehoge12'}
     password_confirmation { password }
     first_name_kanji      { '山田' }
     last_name_kanji       { '太郎' }
