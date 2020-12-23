c2ed2c3e38b4bced7fdbd3ebb0a9d520efe07992
bokuson100 <syk.grazie.50@gmail.com>
2020-12-24 00:21:45 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except:[:index]
+
   def index; end
+
+  def new
+   @item = Item.new
+  end
+  
+  def create
+   @item = Item.new(item_params)
+   if @item.save
+    redirect_to root_path
+   else
+    render :new
+  end
+
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:item_name, :image, :text, :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, :price).merge(user_id: current_user.id)
+end
 end
