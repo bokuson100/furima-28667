c36f9788ed399e6c1a9364dd4fe823f91d24ea54
bokuson100 <syk.grazie.50@gmail.com>
2020-12-24 00:18:35 +0900

    ActiveHashのself.data作成

---
+++ app/models/item.rb
@@ -0,0 +1,19 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category #（コントローラのクラス名と一致）
+  belongs_to :status
+  belongs_to :delivery_burden
+  belongs_to :prefecture
+  belongs_to :delivery_days
+
+  with_options presence: true do
+  validates :image
+  validates :item_name
+  validates :text
+  end
+
+  validates :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, numericality:{ other_than: 0 }#バリデーションはカラム名と一致
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+   
+end
