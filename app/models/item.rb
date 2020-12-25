bcb7e0892c091a98fa872724178895216f1f2758
bokuson100 <syk.grazie.50@gmail.com>
2020-12-25 18:39:00 +0900

    必須バリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,9 @@ class Item < ApplicationRecord
     validates :item_name
     validates :text
   end
-
-  validates :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, numericality: { other_than: 0 } # バリデーションはカラム名と一致
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  
+  with_options presence: true do
+    validates :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, numericality: { other_than: 0 } # バリデーションはカラム名と一致
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
