54cc7784d4d26c3e6f592bd17632abfbd1234cb6
bokuson100 <syk.grazie.50@gmail.com>
2020-12-26 14:43:34 +0900

    18行目　presence:trueの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,6 @@ class Item < ApplicationRecord
     validates :item_name
     validates :text
     validates :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, numericality: { other_than: 0 } # バリデーションはカラム名と一致
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
   end
