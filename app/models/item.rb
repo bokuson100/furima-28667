7c11e290d21617845aa60c02db745992494047ed
bokuson100 <syk.grazie.50@gmail.com>
2020-12-26 13:53:38 +0900

    userとのアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -6,15 +8,13 @@ class Item < ApplicationRecord
   belongs_to :delivery_burden
   belongs_to :prefecture
   belongs_to :delivery_days
+  belongs_to :user
 
   with_options presence: true do
     validates :image
     validates :item_name
     validates :text
-  end
-  
-  with_options presence: true do
     validates :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, numericality: { other_than: 0 } # バリデーションはカラム名と一致
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
-end
+  end
