c3ab186393d25a1898da4f2ee0b59655aacd8c6c
bokuson100 <syk.grazie.50@gmail.com>
2020-12-29 15:56:39 +0900

    コントローラー定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
-    @items = Item.all.order("created_at ASC")
+    @items = Item.all.order('created_at ASC')
   end
 
   def new
@@ -12,17 +12,21 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(params[:id])
     if @item.save
       redirect_to root_path
     else
       render :new
    end
-end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :image, :text, :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :image, :text, :category_id, :delivery_burden_id, :delivery_days_id, :prefecture_id, :status_id, :price).merge(:current_user.id)
   end
 end
