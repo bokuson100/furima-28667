9f44ed63e1682a828fbed6538b660a37af520deb
bokuson100 <syk.grazie.50@gmail.com>
2021-01-07 12:34:48 +0900

    itemモデルのテストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
-    @item.image = fixture_file_upload('public/images/test_image.jpg')
   end
 
   describe '商品出品' do
